a6a9d17e94de14e5af1b7aecde2d56b660c5c66c
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-08-21 00:44:43 +0600

    calculation and modal button added

--- script/app.js
+++ script/app.js
@@ -11,32 +11,60 @@ function handleCLikBtn(target) {
     selectedItemContainer.appendChild(li);
     // console.log(target.childNodes[5].childNodes[3].innerText);
     const priceField = target.childNodes[5].childNodes[3].innerText
-    const priceValueText =priceField.split(" ")[0];
+    const priceValueText = priceField.split(" ")[0];
     const priceValue = parseFloat(priceValueText);
     total = parseFloat(total) + priceValue;
-    document.getElementById('purchase-total-price').innerText= total;
+    document.getElementById('purchase-total-price').innerText = total;
+    document.getElementById('discount-total').innerText= total; 
+    // CUPPON bUTTON
+    const couponButton = document.getElementById("btn-coupon")
+    if (total >= 200) {
+        couponButton.removeAttribute('disabled');
+    } else {
+        couponButton.setAttribute('disabled', true);
+    }
+    // PURCHASE BUTTON
+    const purchaseBtn = document.getElementById("btn-purchase")
+    if (total > 0) {
+        purchaseBtn.removeAttribute('disabled');
+        document.getElementById("discount-total").innerText= total;
+    } else {
+        purchaseBtn.setAttribute('disabled', true);
+    }
+}
+
+// CUPON BUTTON
+
+document.getElementById("btn-coupon").addEventListener('click', function () {
+    const discountText = document.getElementById('discount-field');
+    const discountTotal = document.getElementById('discount-total');
+    const couponCode = document.getElementById("cupon-confirm").value
+    if (couponCode === "SELL200") {
+        const coupon = document.getElementById('coupon');
+        coupon.innerText = 'YOU HAVE RECIVED 20% DISCOUNT';
+        const discountValue = discountText.innerText = parseFloat(total * 0.20).toFixed(2);
+        const finalTotal = discountTotal.innerText = total - discountValue;
+        finalTotal.toFixed(2);
+        document.getElementById("cupon-confirm").value= "";
+    } else {
+        alert("Invalid Coupon")
+    }
+})
+
+function goHome() {
+    // window.location.href = "index.html"
+    total = parseFloat(0);
+    document.getElementById("selected-items").innerText= "";
+    document.getElementById("purchase-total-price").innerText= 0;
+    document.getElementById("discount-field").innerText= 0;
+    document.getElementById("discount-total").innerText= 0;
+    document.getElementById("cupon-confirm").value= "";
+    document.getElementById("btn-purchase").setAttribute('disabled', true);
 }
 
 
-// coupon button active
-document.getElementById('cupon-confirm').addEventListener('keyup', function (event) {
-    const text = event.target.value;
-    const couponButton = document.getElementById('btn-coupon');
-    if (text === 'SELL200') {
-        couponButton.removeAttribute('disabled');
-    }
-    else {
-        couponButton.setAttribute('disabled', true);
-    }
-})
-// Coupon title change
-document.getElementById('btn-coupon').addEventListener('click', function () {
-    const coupon = document.getElementById('coupon');
-    coupon.innerText = 'YOU HAVE RECIVED 20% DISCOUNT';
-    const discount = document.getElementById('discount-field')
-    discount.innerText = totalPrice * 0.20
-})
 
 
-// purchase button active
+
+
 
